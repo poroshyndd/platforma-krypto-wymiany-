7f4c4384d58375c32ddeda72b327c084bf3852b5
poroshyndd <poroshyndd@gmail.com>
2025-06-22 18:29:36 +0200

    Update payments.js

--- backend/src/routes/payments.js
+++ backend/src/routes/payments.js
@@ -1,10 +1,6 @@
-// routes/payments.js
 const express = require('express');
 const router  = express.Router();
 const paymentController = require('../controllers/paymentController');
 
 router.post('/start', paymentController.startPayment);
-// сюда же добавьте вебхук-обработчик, если нужен:
-// router.post('/webhook', paymentController.webhookHandler);
-
 module.exports = router;
