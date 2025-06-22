9498796b8f0455b8f41a96909802955bbc288d80
poroshyndd <poroshyndd@gmail.com>
2025-06-22 18:31:52 +0200

    Update PaymentResult.js

--- frontend/src/pages/PaymentResult.js
+++ frontend/src/pages/PaymentResult.js
@@ -2,13 +2,12 @@ import React, { useEffect } from 'react';
 
 export default function PaymentResult() {
   useEffect(() => {
-    // Тут можно разобрать query params и показать статус
   }, []);
 
   return (
     <div>
-      <h1>Результат оплаты</h1>
-      <p>Спасибо, ваш платёж принят!</p>
+      <h1>Wynik płatności</h1>
+      <p>Dziękujemy, Twoja płatność została przyjęta!</p>
       <a href="/">← Назад на главную</a>
     </div>
   );
